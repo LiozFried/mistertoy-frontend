1f10bf21de9547771cd50dc6fa115e00cc442963
Lioz Fired <friedlioz@gmail.com>
2025-08-26 16:14:58 +0300

    Exporting utils funcs

--- src/services/util.service.js
+++ src/services/util.service.js
@@ -8,7 +8,7 @@ export const utilService = {
     debounce
 }
 
-function makeId(length = 6) {
+export function makeId(length = 6) {
     var txt = ''
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
@@ -19,7 +19,7 @@ function makeId(length = 6) {
     return txt
 }
 
-function makeLorem(size = 100) {
+export function makeLorem(size = 100) {
     var words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
     var txt = ''
     while (size > 0) {
@@ -29,23 +29,23 @@ function makeLorem(size = 100) {
     return txt
 }
 
-function getRandomIntInclusive(min, max) {
+export function getRandomIntInclusive(min, max) {
     min = Math.ceil(min)
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive 
 }
 
-function saveToStorage(key, value) {
+export function saveToStorage(key, value) {
     localStorage.setItem(key, JSON.stringify(value))
 }
 
-function loadFromStorage(key) {
+export function loadFromStorage(key) {
     const data = localStorage.getItem(key)
     return (data) ? JSON.parse(data) : undefined
 }
 
 // In our utilService
-function animateCSS(el, animation) {
+export function animateCSS(el, animation) {
     const prefix = 'animate__'
     return new Promise((resolve, reject) => {
         const animationName = `${prefix}${animation}`
@@ -62,7 +62,7 @@ function animateCSS(el, animation) {
     })
 }
 
-function debounce(func, timeout = 300) {
+export function debounce(func, timeout = 300) {
     let timer
     return (...args) => {
         clearTimeout(timer)
