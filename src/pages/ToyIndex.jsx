24c69534b91fd00357139959305dd6028b76634b
Lioz Fired <friedlioz@gmail.com>
2025-08-26 17:05:01 +0300

    Connect to Index

--- src/pages/ToyIndex.jsx
+++ src/pages/ToyIndex.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 import { useEffect, useState } from "react"
-
+import { ToyFilter } from "../cmps/ToyFIlter"
 import { ToyList } from "../cmps/ToyList"
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 import { loadToys, removeToy, setFilter } from "../store/actions/toy.action"
@@ -36,7 +36,9 @@ export function ToyIndex() {
 
     return (
         <section className="toy-index">
-            <p>filter cmp</p>
+            <ToyFilter
+                filterBy={filterBy} onSetFilter={onSetFilter} toyLabels={toyLabels}
+            />
 
             <button className="add-toy-btn">
                 <Link to="/toy/edit">Add Toy</Link>
