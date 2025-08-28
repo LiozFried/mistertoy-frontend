fcb864893754e2c7e4822d1c961d76c29d270137
Lioz Fired <friedlioz@gmail.com>
2025-08-28 18:22:40 +0300

    Change the page to conditinal rendering

--- src/pages/ToyDashboard.jsx
+++ src/pages/ToyDashboard.jsx
@@ -1,21 +1,32 @@
-import { useEffect, useState } from "react"
-import { LabelsAveragePrice } from "../cmps/LabelsAveragePrice";
-import { LabelsInventory } from "../cmps/LabelsInventory";
-import { RandomLineChart } from "../cmps/RandomLineChart";
+import { useState } from "react"
+import { LabelsAveragePrice } from "../cmps/LabelsAveragePrice"
+import { LabelsInventory } from "../cmps/LabelsInventory"
+import { RandomLineChart } from "../cmps/RandomLineChart"
 
 export function ToyDashboard() {
+    const [activeChart, setActiveChart] = useState(null)
+
+    function renderChart() {
+        switch (activeChart) {
+            case 'average-price':
+                return <LabelsAveragePrice />
+            case 'inventory':
+                return <LabelsInventory />
+            case 'line-chart':
+                return <RandomLineChart />
+            default:
+                return null
+        }
+    }
 
     return (
         <section className="toy-dashboard">
             <div className="dashboard-btns">
-                <button>Labels Average Price</button>
-                <button></button>
-                <button></button>
+                <button onClick={() => setActiveChart('average-price')}>Labels Average Price</button>
+                <button onClick={() => setActiveChart('inventory')}>Labels Inventory</button>
+                <button onClick={() => setActiveChart('line-chart')}>Random Line Chart</button>
             </div>
-
-            <LabelsAveragePrice />
-            <LabelsInventory />
-            <RandomLineChart />
+            {renderChart()}
         </section>
     )
 }
