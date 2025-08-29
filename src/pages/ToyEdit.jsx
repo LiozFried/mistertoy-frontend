90e6075abfac4cc1be6639ea43920650ffb8eb32
Lioz Fired <friedlioz@gmail.com>
2025-08-29 16:26:43 +0300

    Use validation with Formik and Yup in edit component

--- src/pages/ToyEdit.jsx
+++ src/pages/ToyEdit.jsx
@@ -4,6 +4,24 @@ import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service"
 import { toyService } from "../services/toy.service"
 import { saveToy } from "../store/actions/toy.action"
 import { useSelector } from "react-redux"
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+
+const ToySchema = Yup.object().shape({
+    name: Yup.string()
+        .min(3, 'Toy name must be at least 3 characters long')
+        .max(50, 'Toy name cannot be more then 50 characters')
+        .required('Required'),
+    price: Yup.number()
+        .min(5, 'Minimum price is $5')
+        .required('Required')
+        .typeError('Must be a number'),
+    labels: Yup.array()
+        .min(1, 'You must choose at least one label')
+        .max(4, 'You can choose maximum of 4 labels')
+        .required('Required'),
+    inStock: Yup.boolean()
+})
 
 export function ToyEdit() {
     const [toyToEdit, setToyToEdit] = useState(toyService.getEmptyToy())
@@ -13,10 +31,13 @@ export function ToyEdit() {
 
     useEffect(() => {
         loadToy()
-    }, [])
+    }, [toyId])
 
     function loadToy() {
-        if (!toyId) return
+        if (!toyId) {
+            setToyToEdit(toyService.getEmptyToy())
+            return
+        }
         toyService.getById(toyId)
             .then(toy => setToyToEdit(toy))
             .catch(err => {
@@ -26,23 +47,23 @@ export function ToyEdit() {
             })
     }
 
-    function handleChange({ target }) {
-        const { name, value, type, checked } = target
-        let fieldValue = value
-        if (type === 'checkbox') {
-            fieldValue = checked
-        } else if (type === 'number') {
-            fieldValue = +value
-        } else if (type === 'select-multiple') {
-            fieldValue = [...target.selectedOptions].map(option => option.value)
-        }
+    //Handle Change for old form:
+    // function handleChange({ target }) {
+    //     const { name, value, type, checked } = target
+    //     let fieldValue = value
+    //     if (type === 'checkbox') {
+    //         fieldValue = checked
+    //     } else if (type === 'number') {
+    //         fieldValue = +value
+    //     } else if (type === 'select-multiple') {
+    //         fieldValue = [...target.selectedOptions].map(option => option.value)
+    //     }
 
-        setToyToEdit(prevToy => ({ ...prevToy, [name]: fieldValue }))
-    }
+    //     setToyToEdit(prevToy => ({ ...prevToy, [name]: fieldValue }))
+    // }
 
-    function onSaveToy(ev) {
-        ev.preventDefault()
-        saveToy(toyToEdit)
+    function onSaveToy(value) {
+        saveToy(value)
             .then((savedToy) => {
                 showSuccessMsg(`Toy ${savedToy._id} saved`)
                 navigate('/toy')
@@ -53,56 +74,55 @@ export function ToyEdit() {
             })
     }
 
+    if (!toyToEdit) return <div>Loading...</div>
+
     return (
         <section className="toy-edit">
             <h1>{toyToEdit._id ? 'Edit' : 'Add'} Toy</h1>
-            <form onSubmit={onSaveToy}>
-                <div className="form-group">
-                    <label htmlFor="name">Name:</label>
-                    <input
-                        type="text" id="name" name="name"
-                        value={toyToEdit.name} onChange={handleChange}
-                        required
-                    />
-                </div>
+            <Formik
+                initialValues={toyToEdit}
+                validationSchema={ToySchema}
+                onSubmit={onSaveToy}
+                enableReinitialize={true}
+            >
+                {({ value }) => (
+                    <Form>
+                        <div className="form-group">
+                            <label htmlFor="name">Name:</label>
+                            <Field type="text" id="name" name="name" />
+                            <ErrorMessage name="name" component="div" className="error-msg" />
+                        </div>
 
-                <div className="form-group">
-                    <label htmlFor="price">Price</label>
-                    <input
-                        type="number" name="price" id="price"
-                        value={toyToEdit.price} onChange={handleChange}
-                        required min={"1"}
-                    />
-                </div>
+                        <div className="form-group">
+                            <label htmlFor="price">Price</label>
+                            <Field type="number" id="price" name="price" />
+                            <ErrorMessage name="price" component="div" className="error-msg" />
+                        </div>
 
-                <div className="form-group">
-                    <label htmlFor="labels">Labels:</label>
-                    <select
-                        name="labels" id="labels"
-                        multiple
-                        value={toyToEdit.labels} onChange={handleChange}
-                    >
-                        {labels.map(label => (
-                            <option key={label} value={label}>
-                                {label}
-                            </option>
-                        ))}
-                    </select>
-                </div>
+                        <div className="form-group">
+                            <label htmlFor="labels">Labels:</label>
+                            <Field as="select" multiple id="labels" name="labels" >
+                                {labels.map(label => (
+                                    <option key={label} value={label}>
+                                        {label}
+                                    </option>
+                                ))}
+                            </Field>
+                            <ErrorMessage name="labels" component="div" className="error-msg" />
+                        </div>
 
-                {toyToEdit._id && (
-                    <div className="form-group">
-                        <label htmlFor="inStock">In Stock:</label>
-                        <input
-                            type="checkbox" name="inStock" id="inStock"
-                            checked={toyToEdit.inStock}
-                            onChange={handleChange}
-                        />
-                    </div>
+                        {toyToEdit._id && (
+                            <div className="form-group">
+                                <label htmlFor="inStock">In Stock:</label>
+                                <Field type="checkbox" id="inStock" name="inStock"/>
+                            </div>
+                        )}
+
+                        <button type="submit">{toyToEdit._id ? 'Update Toy' : 'Add'}</button>
+                    </Form>
                 )}
+            </Formik>
 
-                <button>{toyToEdit._id ? 'Update Toy' : 'Add'}</button>
-            </form>
         </section>
     )
 }
