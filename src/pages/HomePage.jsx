6d6b403551443dee4adb767b8907378d56adff38
Lioz Fired <friedlioz@gmail.com>
2025-09-01 18:13:41 +0300

    Fix home page styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import heroImg from '../assets/img/HERO_IMG.png'
 export function HomePage() {
     return (
         <section className='home-page'>
+            <h1>Welcome to our store</h1>
             <img
                 src={heroImg}
                 alt='Hero Image'
